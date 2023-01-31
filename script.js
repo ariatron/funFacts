5b4f5017e9e6e551febeb638697d677b68251f88
Ariana <ariana.r.lloyd@gmail.com>
2023-01-30 22:27:28 -0600

    Updating script.js
    Added ascii art, fixed honedSkills

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const randomNumFun = num => {return Math.floor(Math.random() * num)
 // object literal
 const funNonsense = {
     timePeriod: ['1400s', '1500s', '100 BC', '2000s', '1772'],
-    honedSkill: ['archery', 'dancing', 'horseback riding', 'sailing', 'farming'],
+    honedSkills: ['archery', 'dancing', 'horseback riding', 'sailing', 'farming'],
     survivalRate: ['dead by 25 (hard life)', 'old age', 'events unknown', 'heart attack', 'the fever']
 };
 
 // Store 'history' in an array
 let tempStorage = [];
 
-// iterate through the object
+// iterate through the object funNonsense
 for (let i in funNonsense) {
     let queueIndex = randomNumFun(funNonsense[i].length)
     console.log(i);
@@ -20,10 +20,10 @@ for (let i in funNonsense) {
     // Customize the output
     switch(i) {
         case 'timePeriod':
-            tempStorage.push(`Your oldest documented ancestor is from "${funNonsense[i][queueIndex]}".`)
+            tempStorage.push(`Your oldest documented ancestor is from ${funNonsense[i][queueIndex]}.`)
             break;
         case 'honedSkills':
-            tempStorage.push(`You have an ancestor who was known for ${funNonsense[i][queueIndex]}".`)
+            tempStorage.push(`You have an ancestor who was known for ${funNonsense[i][queueIndex]}.`)
             break;
         case 'survivalRate':
             tempStorage.push(`There was one ancestor whose cause of death was ${funNonsense[i][queueIndex]}.`)
@@ -34,9 +34,15 @@ for (let i in funNonsense) {
 }
 
 function formatFunNonsense(details) {
-    //
-    //
+    const cat = [
+        ' /\\_/\\ ',
+        '( o.o )',
+        ' > ^ < '
+    ];  
     const formatted = tempStorage.join('\n')
+    for (const row of cat) {
+        console.log(row);
+    }
     console.log(formatted);
 }
 
